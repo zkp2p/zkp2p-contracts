404d968124fdf7068ce0727d72f8cdf53021cd5f
Richard <richardliang2015@u.northwestern.edu>
2026-01-08 12:30:56 +0800

    deploy: chime (#96)
    * deploy: chime
    
    * chore: rm continuity
    
    * chore: bump

---
+++ deployments/verifiers/chime.ts
@@ -0,0 +1,13 @@
+import { BigNumber, ethers } from "ethers";
+import { calculatePaymentMethodHash, Currency } from "@utils/protocolUtils";
+
+export const CHIME_PAYMENT_METHOD_HASH = calculatePaymentMethodHash("chime");
+
+export const CHIME_CURRENCIES: any = [
+  Currency.USD,
+];
+
+export const CHIME_PROVIDER_CONFIG = {
+  paymentMethodHash: CHIME_PAYMENT_METHOD_HASH,
+  currencies: CHIME_CURRENCIES
+};
