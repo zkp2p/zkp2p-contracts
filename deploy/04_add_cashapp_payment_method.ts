4da6c285c1d42c117bf7a485c4aeef251da3b765
Sachin <0xsachink@gmail.com>
2026-02-20 16:18:28 +0700

    chore: freeze V1 payment method deploy scripts (02-12)
    All V1 payment method scripts now unconditionally skip. Payment methods
    are managed by 22_configure_v2_payment_methods going forward.

--- deploy/04_add_cashapp_payment_method.ts
+++ deploy/04_add_cashapp_payment_method.ts
@@ -63,7 +63,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 
 func.skip = async (hre: HardhatRuntimeEnvironment): Promise<boolean> => {
-  return false;
+  return true; // Frozen: payment methods are now managed by 22_configure_v2_payment_methods
 };
 
 export default func;
