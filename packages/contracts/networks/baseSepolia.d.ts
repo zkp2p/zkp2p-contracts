a525914beb5a93682b1ccebd10877988eb425258
Sachin <0xsachink@gmail.com>
2026-02-25 19:34:35 +0700

    chore: bump SDK to 0.1.11-rc.3, remove baseSepolia network files

--- packages/contracts/networks/baseSepolia.d.ts
+++
@@ -1,5 +0,0 @@
-// Auto-generated types for network baseSepolia
-export { default as addresses } from '../addresses/baseSepolia';
-export * from '../abis/baseSepolia';
-export { default as constants } from '../constants/baseSepolia';
-export { default as paymentMethods } from '../paymentMethods/baseSepolia';
