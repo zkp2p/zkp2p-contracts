c6420ccca4d03046566850c79685b1685862adde
Sachin <0xsachink@gmail.com>
2026-02-25 19:34:35 +0700

    chore: bump SDK to 0.1.11-rc.3, remove baseSepolia network files

--- packages/contracts/networks/baseSepolia.mjs
+++
@@ -1,5 +0,0 @@
-// Auto-generated network entry for baseSepolia
-export { default as addresses } from '../_esm/addresses/baseSepolia.js';
-export * from '../_esm/abis/baseSepolia/index.js';
-export { default as constants } from '../_esm/constants/baseSepolia.js';
-export { default as paymentMethods } from '../_esm/paymentMethods/baseSepolia.js';
